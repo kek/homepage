ab1bd81b17795be0eb94a3f9179e54030381b831
Karl Eklund <localpart@gmail.com>
2017-04-18 23:36:34 +0200

    Simple Erlang install script

---
+++ install_erlang.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install wget
+sudo apt-get install libssl-dev
+sudo apt-get install ncurses-dev
+wget http://www.erlang.org/download/otp_src_19.3.tar.gz
+tar -xzvf otp_src_19.3.tar.gz
+cd otp_src_19.3/
+./configure
+make
+sudo make install
+cd ..
+rm otp_src_19.3.tar.gz
