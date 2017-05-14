eac5763675196dadad4d02ae62f244324c6ac185
Karl Eklund <localpart@gmail.com>
2017-05-14 23:25:37 +0200

    Presence list

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,15 @@ defmodule Hello.PageController do
   use Hello.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    nodes = [:harry, :ingrid]
+    pings = nodes
+    |> Enum.map(&(:"hello@#{&1}.local"))
+    |> IO.inspect
+    |> Enum.map(&Node.ping/1)
+
+    present = Enum.zip(nodes, pings)
+    |> Map.new
+
+    render conn, "index.html", present: present
   end
 end
