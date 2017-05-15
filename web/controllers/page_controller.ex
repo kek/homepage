540558b08dbf10d53769c209152dd4215ce6bab5
Karl Eklund <localpart@gmail.com>
2017-05-15 18:41:16 +0200

    clean up

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,13 +2,13 @@ defmodule Hello.PageController do
   use Hello.Web, :controller
 
   def index(conn, _params) do
-    nodes = [:harry, :ingrid]
-    pings = nodes
+    names = [:harry, :ingrid]
+
+    pings = names
     |> Enum.map(&(:"hello@#{&1}.local"))
-    |> IO.inspect
     |> Enum.map(&Node.ping/1)
 
-    present = Enum.zip(nodes, pings)
+    present = Enum.zip(names, pings)
     |> Map.new
 
     render conn, "index.html", present: present
