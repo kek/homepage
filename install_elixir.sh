cd8d327f52154f9339a24debbedf0af1ae7bab5f
Karl Eklund <localpart@gmail.com>
2017-06-21 22:00:31 +0200

    Use Elixir v1.4.4 instead of master

--- install_elixir.sh
+++ install_elixir.sh
@@ -2,7 +2,11 @@
 
 git clone https://github.com/elixir-lang/elixir
 cd elixir
-make clean test
+git fetch --all
+git checkout master
+git pull
+git checkout v1.4.4
+make clean test && sudo make install
 
 # mix local.hex
 # mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
