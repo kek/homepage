1b5244f2afbf0f6e64edaed2e6e9c2aca2cfe334
Karl Eklund <localpart@gmail.com>
2017-06-25 15:34:32 +0200

    Erlang 20.0

--- install_erlang.sh
+++ install_erlang.sh
@@ -3,11 +3,11 @@ sudo apt-get update
 sudo apt-get install wget
 sudo apt-get install libssl-dev
 sudo apt-get install ncurses-dev
-wget http://www.erlang.org/download/otp_src_19.3.tar.gz
-tar -xzvf otp_src_19.3.tar.gz
-cd otp_src_19.3/
+wget http://www.erlang.org/download/otp_src_20.0.tar.gz
+tar -xzvf otp_src_20.0.tar.gz
+cd otp_src_20.0/
 ./configure
 make
 sudo make install
 cd ..
-rm otp_src_19.3.tar.gz
+rm otp_src_20.0.tar.gz
