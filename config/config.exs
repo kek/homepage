ca13ced94a2c49680b4658760695d5c008800f51
Karl Eklund <localpart@gmail.com>
2018-01-04 13:42:30 +0100

    Formatting

--- config/config.exs
+++ config/config.exs
@@ -6,16 +6,14 @@
 use Mix.Config
 
 # General application configuration
-config :hello,
-  namespace: Hello
+config :hello, namespace: Hello
 
 # Configures the endpoint
 config :hello, Hello.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "gJATC1LVk4KuW6zj2CfhBdkWei5/p/JYMilDOqYQMwrMZxQr4/Lgf8gLMHXZcgNp",
   render_errors: [view: Hello.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Hello.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Hello.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -24,4 +22,4 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
