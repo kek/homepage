a28f18c22dedace47d2f657efc4b79045a6af858
Karl Eklund <localpart@gmail.com>
2018-01-04 10:36:59 +0100

    New Erlang and Elixir versions

--- install-erlang.sh
+++ install-erlang.sh
@@ -3,11 +3,11 @@ sudo apt-get update
 sudo apt-get install wget
 sudo apt-get install libssl-dev
 sudo apt-get install ncurses-dev
-wget http://www.erlang.org/download/otp_src_20.0.tar.gz
-tar -xzvf otp_src_20.0.tar.gz
-cd otp_src_20.0/
+wget http://www.erlang.org/download/otp_src_20.1.tar.gz
+tar -xzvf otp_src_20.1.tar.gz
+cd otp_src_20.1/
 ./configure
 make
 sudo make install
 cd ..
-rm otp_src_20.0.tar.gz
+rm otp_src_20.1.tar.gz
