d866ce455e72d7c37fcd901a25286421d80be7e1
Karl Eklund <localpart@gmail.com>
2018-03-08 01:21:42 +0100

    Upgrade stuff and add wobserver

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hello.Mixfile do
   def project do
     [
       app: :hello,
-      version: "0.0.4",
+      version: "0.0.5",
       elixir: "~> 1.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
@@ -41,14 +41,15 @@ defmodule Hello.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.2.1"},
+      {:phoenix, "~> 1.3"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_html, "~> 2.6"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
       {:edeliver, "~> 1.4"},
-      {:distillery, "~> 1.5", warn_missing: false}
+      {:distillery, "~> 1.5", warn_missing: false},
+      {:wobserver, "~> 0.1"}
     ]
   end
 end
