49ec550c11d67aaaaa05c9f9eb05660ada6f3959
Karl Eklund <localpart@gmail.com>
2023-10-14 10:05:20 +0200

    Build in my account

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh pi@pablo.local "git clone https://github.com/kek/rpi-elixir-cluster rpi; cd rpi; git status; git checkout -- .; git pull; sh build.sh; rsync -r _build/prod/rel/hello/* ~/hello; sudo systemctl restart hello"
+ssh pablo.local "git clone https://github.com/kek/rpi-elixir-cluster rpi; cd rpi; git status; git checkout -- .; git pull; sh build.sh; sudo rsync -r _build/prod/rel/hello/* ~pi/hello; sudo systemctl restart hello"
