16aa99d980888ad9465ae87ebc127eb65129b9b8
Karl Eklund <localpart@gmail.com>
2023-10-16 01:49:52 +0200

    Organize files around

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ssh pablo.local "git clone https://github.com/kek/sofa; cd sofa; git status; git checkout -- .; git pull; sh build.sh; sudo rsync -r _build/prod/rel/hello/* ~pi/hello; sudo systemctl restart hello"
