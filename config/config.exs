84b4c953263e53cd75c3c7f85d636ba86ee82629
Karl Eklund <localpart@gmail.com>
2023-10-16 13:52:08 +0200

    Enable server

--- config/config.exs
+++ config/config.exs
@@ -19,7 +19,8 @@ config :hello, Hello.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "gJATC1LVk4KuW6zj2CfhBdkWei5/p/JYMilDOqYQMwrMZxQr4/Lgf8gLMHXZcgNp",
   render_errors: [view: Hello.ErrorView, accepts: ~w(html json)],
-  pubsub_server: Hello.PubSub
+  pubsub_server: Hello.PubSub,
+  server: true
 
 # Configures Elixir's Logger
 config :logger, :console,
