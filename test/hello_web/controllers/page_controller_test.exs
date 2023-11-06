3af9c702a82f15faa67084a8af3d06926cd0420e
Karl Eklund <localpart@gmail.com>
2023-11-06 20:13:15 +0100

    Update tests from scaffold project

--- test/hello_web/controllers/page_controller_test.exs
+++ test/hello_web/controllers/page_controller_test.exs
@@ -1,8 +1,8 @@
-defmodule Hello.PageControllerTest do
-  use Hello.ConnCase
+defmodule HelloWeb.PageControllerTest do
+  use HelloWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
+    conn = get(conn, ~p"/")
     assert html_response(conn, 200) =~ "DOCTYPE html"
   end
 end
