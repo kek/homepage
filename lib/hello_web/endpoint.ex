e9b4e5592678aece60ed15e125d3711087be7d98
Karl Eklund <localpart@gmail.com>
2023-11-06 20:23:41 +0100

    Remove last (?) trace of Ecto

--- lib/hello_web/endpoint.ex
+++ lib/hello_web/endpoint.ex
@@ -30,7 +30,6 @@ defmodule HelloWeb.Endpoint do
     socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
     plug(Phoenix.LiveReloader)
     plug(Phoenix.CodeReloader)
-    plug(Phoenix.Ecto.CheckRepoStatus, otp_app: :hello)
   end
 
   plug(Phoenix.LiveDashboard.RequestLogger,
