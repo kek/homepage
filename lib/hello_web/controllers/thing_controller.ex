dc52f87766d6f8cdcdc3f2ac4b2304f0422e684d
Karl Eklund <localpart@gmail.com>
2023-11-27 00:22:57 +0100

    Read things from server

--- lib/hello_web/controllers/thing_controller.ex
+++ lib/hello_web/controllers/thing_controller.ex
@@ -2,7 +2,13 @@ defmodule HelloWeb.ThingController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
-    things = [%{title: "Thing 1"}, %{title: "Thing 2"}]
-    json(conn, things)
+    things = [
+      %{title: "Thing 1", description: "yes"},
+      %{title: "Thing 2", description: "maybe not"}
+    ]
+
+    conn
+    |> put_resp_header("access-control-allow-origin", "*")
+    |> json(things)
   end
 end
--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,10 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, p, text)
+import Html exposing (Html, div, h2, p, text)
 import Http
+import Json.Decode exposing (Decoder, field, map2, string)
+import List exposing (map)
 
 
 
@@ -12,11 +14,46 @@ import Http
 main : Program () Model Msg
 main =
     Browser.element
-        { init = \_ -> ( [], Cmd.none )
+        { init = init
         , view = view
         , update = update
         , subscriptions = \_ -> Sub.none
         }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( noContent, getData )
+
+
+noContent : Model
+noContent =
+    { items = [], error = Nothing }
+
+
+successContent : List Item -> Model
+successContent value =
+    { items = value, error = Nothing }
+
+
+errorContent : List Item -> Http.Error -> Model
+errorContent items _ =
+    { items = items, error = Just "Something went wrong." }
+
+
+getData : Cmd Msg
+getData =
+    Http.get
+        { url = "http://localhost:4000/things"
+        , expect = Http.expectJson DataReceived (Json.Decode.list itemDecoder)
+        }
+
+
+itemDecoder : Decoder Item
+itemDecoder =
+    map2 Item
+        (field "title" string)
+        (field "description" string)
 
 
 
@@ -24,13 +61,15 @@ main =
 
 
 type alias Item =
-    { name : String
+    { title : String
     , description : String
     }
 
 
 type alias Model =
-    List Item
+    { items : List Item
+    , error : Maybe String
+    }
 
 
 
@@ -38,15 +77,17 @@ type alias Model =
 
 
 type Msg
-    = SendHttpRequest
-    | DataReceived (Result Http.Error String)
+    = DataReceived (Result Http.Error (List Item))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg _ =
+update msg model =
     case msg of
-        _ ->
-            ( [], Cmd.none )
+        DataReceived (Ok value) ->
+            ( successContent value, Cmd.none )
+
+        DataReceived (Err error) ->
+            ( errorContent model.items error, Cmd.none )
 
 
 
@@ -54,6 +95,15 @@ update msg _ =
 
 
 view : Model -> Html Msg
-view _ =
-    div []
-        [ p [] [ text "Hello, World!" ] ]
+view model =
+    case model.error of
+        Just error ->
+            div [] [ text error ]
+
+        Nothing ->
+            div [] (map viewItem model.items)
+
+
+viewItem : Item -> Html Msg
+viewItem item =
+    div [] [ h2 [] [ text item.title ], p [] [ text item.description ] ]
