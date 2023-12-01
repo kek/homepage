7f546b84a17ea24c42ad7b05f5d629a5a0261bc7
Karl Eklund <localpart@gmail.com>
2023-12-01 11:24:56 +0100

    Don't compile Elm on ARM32 because it doesn't exist

--- lib/mix/tasks/compile/elm.ex
+++ lib/mix/tasks/compile/elm.ex
@@ -2,12 +2,21 @@ defmodule Mix.Tasks.Compile.Elm do
   use Mix.Task
 
   def run(_) do
-    with {_output, 0} <-
-           System.cmd("elm", ["make", "src/Main.elm", "--optimize", "--output=assets/elm/main.js"]) do
-      :ok
+    if {"armv7l\n", 0} == System.cmd("uname", ["-m"]) do
+      IO.puts("Skipping Elm compilation on ARM 32")
     else
-      {output, status} ->
-        {:error, [message: "Elm compilation failed", status: status, output: output]}
+      with {_output, 0} <-
+             System.cmd("elm", [
+               "make",
+               "src/Main.elm",
+               "--optimize",
+               "--output=assets/elm/main.js"
+             ]) do
+        :ok
+      else
+        {output, status} ->
+          {:error, [message: "Elm compilation failed", status: status, output: output]}
+      end
     end
   end
 end
