7b75241eec15cbfcc5ba70eaa3662bede8b83ff3
Karl Eklund <localpart@gmail.com>
2023-12-13 12:11:49 +0100

    Remove styles

--- lib/hello_web/controllers/thing_controller.ex
+++ lib/hello_web/controllers/thing_controller.ex
@@ -2,11 +2,18 @@ defmodule HelloWeb.ThingController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
-    content_directory = "/Users/ke/Documents/gemini/content"
+    content_directories = [
+      "/Users/ke/Documents/gemini/content",
+      "/mnt/c/Users/karle/Documents/gemini/content",
+      "/home/ke/gemini/content"
+    ]
+
+    content_directory = Enum.find(content_directories, &File.exists?/1)
 
     things =
-      if File.exists?(content_directory) do
+      if content_directory do
         File.ls!(content_directory)
+        |> Enum.reject(&(&1 == "index.gmi"))
         |> Enum.map(fn file ->
           file_path = Path.join(content_directory, file)
           file_contents = File.read!(file_path)
@@ -14,8 +21,8 @@ defmodule HelloWeb.ThingController do
         end)
       else
         [
-          %{title: "Thing 1", description: "yes\n\nno"},
-          %{title: "Thing 2", description: "maybe not\n\nor perhaps"}
+          %{title: "A wonderful headline", description: "yes\n\nno"},
+          %{title: "The best section title ever", description: "maybe not\n\nor perhaps"}
         ]
       end
 
--- src/Main.elm
+++ src/Main.elm
@@ -2,10 +2,12 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (Html, a, div, h1, h2, li, p, text, ul)
-import Html.Attributes exposing (class)
+import Html.Attributes as Attr exposing (class)
 import Http
 import Json.Decode exposing (Decoder, field, map2, string)
 import List exposing (map)
+import Markdown.Block as Block
+import Markdown.Html
 import Markdown.Parser
 import Markdown.Renderer
 import Parser exposing (Problem)
@@ -167,8 +169,8 @@ viewItem : Item -> Html Msg
 viewItem item =
     li []
         [ h2 [] [ text item.title ]
-        , p []
-            (case render Markdown.Renderer.defaultHtmlRenderer item.description of
+        , div []
+            (case render articleRenderer item.description of
                 Ok html ->
                     html
 
@@ -176,3 +178,191 @@ viewItem item =
                     [ text error ]
             )
         ]
+
+
+articleRenderer : Markdown.Renderer.Renderer (Html msg)
+articleRenderer =
+    { heading =
+        \{ level, children } ->
+            case level of
+                Block.H1 ->
+                    Html.h3 [ class "h-32" ] children
+
+                Block.H2 ->
+                    Html.h4 [ class "h-24" ] children
+
+                Block.H3 ->
+                    Html.h5 [] children
+
+                Block.H4 ->
+                    Html.h6 [] children
+
+                Block.H5 ->
+                    Html.h6 [] children
+
+                Block.H6 ->
+                    Html.h6 [] children
+    , paragraph = Html.p [ class "h-20" ]
+    , hardLineBreak = Html.br [] []
+    , blockQuote = Html.blockquote []
+    , strong =
+        \children -> Html.strong [] children
+    , emphasis =
+        \children -> Html.em [] children
+    , strikethrough =
+        \children -> Html.del [] children
+    , codeSpan =
+        \content -> Html.code [] [ Html.text content ]
+    , link =
+        \link content ->
+            case link.title of
+                Just title ->
+                    Html.a
+                        [ Attr.href link.destination
+                        , Attr.title title
+                        ]
+                        content
+
+                Nothing ->
+                    Html.a [ Attr.href link.destination ] content
+    , image =
+        \imageInfo ->
+            case imageInfo.title of
+                Just title ->
+                    Html.img
+                        [ Attr.src imageInfo.src
+                        , Attr.alt imageInfo.alt
+                        , Attr.title title
+                        ]
+                        []
+
+                Nothing ->
+                    Html.img
+                        [ Attr.src imageInfo.src
+                        , Attr.alt imageInfo.alt
+                        ]
+                        []
+    , text =
+        Html.text
+    , unorderedList =
+        \items ->
+            Html.ul []
+                (items
+                    |> List.map
+                        (\item ->
+                            case item of
+                                Block.ListItem task children ->
+                                    let
+                                        checkbox : Html msg
+                                        checkbox =
+                                            case task of
+                                                Block.NoTask ->
+                                                    Html.text ""
+
+                                                Block.IncompleteTask ->
+                                                    Html.input
+                                                        [ Attr.disabled True
+                                                        , Attr.checked False
+                                                        , Attr.type_ "checkbox"
+                                                        ]
+                                                        []
+
+                                                Block.CompletedTask ->
+                                                    Html.input
+                                                        [ Attr.disabled True
+                                                        , Attr.checked True
+                                                        , Attr.type_ "checkbox"
+                                                        ]
+                                                        []
+                                    in
+                                    Html.li [] (checkbox :: children)
+                        )
+                )
+    , orderedList =
+        \startingIndex items ->
+            Html.ol
+                (case startingIndex of
+                    1 ->
+                        [ Attr.start startingIndex ]
+
+                    _ ->
+                        []
+                )
+                (items
+                    |> List.map
+                        (\itemBlocks ->
+                            Html.li []
+                                itemBlocks
+                        )
+                )
+    , html = Markdown.Html.oneOf []
+    , codeBlock =
+        \{ body, language } ->
+            let
+                classes : List (Html.Attribute msg)
+                classes =
+                    -- Only the first word is used in the class
+                    case Maybe.map String.words language of
+                        Just (actualLanguage :: _) ->
+                            [ Attr.class <| "language-" ++ actualLanguage ]
+
+                        _ ->
+                            []
+            in
+            Html.pre []
+                [ Html.code classes
+                    [ Html.text body
+                    ]
+                ]
+    , thematicBreak = Html.hr [] []
+    , table = Html.table []
+    , tableHeader = Html.thead []
+    , tableBody = Html.tbody []
+    , tableRow = Html.tr []
+    , tableHeaderCell =
+        \maybeAlignment ->
+            let
+                attrs : List (Html.Attribute msg)
+                attrs =
+                    maybeAlignment
+                        |> Maybe.map
+                            (\alignment ->
+                                case alignment of
+                                    Block.AlignLeft ->
+                                        "left"
+
+                                    Block.AlignCenter ->
+                                        "center"
+
+                                    Block.AlignRight ->
+                                        "right"
+                            )
+                        |> Maybe.map Attr.align
+                        |> Maybe.map List.singleton
+                        |> Maybe.withDefault []
+            in
+            Html.th attrs
+    , tableCell =
+        \maybeAlignment ->
+            let
+                attrs : List (Html.Attribute msg)
+                attrs =
+                    maybeAlignment
+                        |> Maybe.map
+                            (\alignment ->
+                                case alignment of
+                                    Block.AlignLeft ->
+                                        "left"
+
+                                    Block.AlignCenter ->
+                                        "center"
+
+                                    Block.AlignRight ->
+                                        "right"
+                            )
+                        |> Maybe.map Attr.align
+                        |> Maybe.map List.singleton
+                        |> Maybe.withDefault []
+            in
+            Html.td attrs
+    }
