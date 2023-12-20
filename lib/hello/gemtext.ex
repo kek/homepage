aa0cf74586aa5ef6d4da12a48e2dc5be4427f46b
Karl Eklund <localpart@gmail.com>
2023-12-21 00:15:20 +0100

    refactor

--- lib/hello/gemtext.ex
+++ lib/hello/gemtext.ex
@@ -1,17 +1,38 @@
+defmodule Hello.Gemtext.Helpers do
+  import NimbleParsec
+
+  def link_marker(combinator \\ empty()) do
+    combinator
+    |> string("=> ")
+    |> ignore()
+  end
+
+  def filename(combinator \\ empty()) do
+    combinator
+    |> ascii_string([?a..?z, ?-], min: 1)
+    |> string(".gmi")
+    |> wrap()
+    |> map({Enum, :join, []})
+    |> tag(:file)
+  end
+
+  def title(combinator \\ empty()) do
+    combinator
+    |> tag(ascii_string([?a..?z, 32, ?A..?Z], min: 1), :title)
+  end
+end
+
 defmodule Hello.Gemtext do
   import NimbleParsec
+  import Hello.Gemtext.Helpers
 
   defparsec(
     :links,
     times(
-      ignore(string("=> "))
-      |> ascii_string([?a..?z, ?-], min: 1)
-      |> string(".gmi")
-      |> wrap()
-      |> map({Enum, :join, []})
-      |> tag(:file)
+      link_marker()
+      |> filename()
       |> ignore(string(" "))
-      |> tag(ascii_string([?a..?z, 32, ?A..?Z], min: 1), :title)
+      |> title()
       |> ignore(string("\n"))
       |> wrap(),
       min: 1
